b132f4facdbb3b124e261ddae78c1254e93a9e2c
ashutosh129520 <ashutosh.saini1295@gmail.com>
2023-12-28 00:42:47 +0530

    new changes

---
+++ Framework/Framework/FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworkApp.swift
+//  Framework
+//
+//  Created by Ashutosh Saini on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
